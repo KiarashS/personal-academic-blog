47141ba2cb9770ab358bac9f631d1631078ab03f
Kiarash Soleimanzadeh <1054134+KiarashS@users.noreply.github.com>
2026-09-15 10:09:48 +0330

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -268,7 +268,7 @@ export const siteConfig: SiteConfig = {
     tagline: 'A curious mind working on AI for health',
     description: 'Kiarash Soleimanzadeh: a curious mind working on AI for health.',
     profileLinks: true,
-    profileLinkStyle: 'both',
+    profileLinkStyle: 'icon', //both, icon, label
     signature: true,
     signatureTilt: -3,
     avatar: '/avatar.jpg',
